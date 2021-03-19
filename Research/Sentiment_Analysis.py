171d8972623ac19c7cad2f629d4476d7131316af
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-03-19 11:42:00 +0000

    Update

--- Research/Sentiment_Analysis.py
+++ Research/Sentiment_Analysis.py
@@ -1,6 +1,49 @@
 from textblob import TextBlob
 # Sentence = input("Say a sentence: ")
-Sentence2 = TextBlob("Doge coin is cool")
+Sentence2 = TextBlob("I am short on TSLA")
 # textblob = TextBlob(Sentence)
-# print(textblob.sentiment)
 print(Sentence2.sentiment)
+
+Hypescore = 1
+text = 'TSLA is an increadibly bad company'
+
+
+
+
+
+
+
+
+
+
+
+# with open(r'Research\MoreTickers.txt', 'r') as f:
+#         lines = f.readlines(1663)
+
+#         lines = [x.strip() for x in lines]
+
+
+
+# for word in text.split():
+
+#             for Ticker in lines:
+
+#                 if word.find(Ticker) == -1:
+#                     pass
+    
+#                 else:
+#                     if TextBlob(text).sentiment.polarity > 0: #If Sentiment is Positive
+#                         print('+1')
+
+#                     if TextBlob(text).sentiment.polarity < 0: #If Sentiment is Negative
+#                         print('-1')
+                    
+#                     if TextBlob(text).sentiment.polarity == 0: #If Sentiment is neutral
+#                         print('+-0')
+
+
+
+
+
+
+
