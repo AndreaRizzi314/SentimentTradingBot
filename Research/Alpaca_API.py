4c4d7bed23b403e082bdeb1cc4439d81e59d54a8
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-03-19 13:51:06 +0000

    Added Environment Variables

--- Research/Alpaca_API.py
+++ Research/Alpaca_API.py
@@ -1,8 +1,12 @@
 import alpaca_trade_api as Trade_api
+import os
+
+KEY = os.environ.get('ALPACA_KEY')
+SECRET = os.environ.get('ALPACA_SECRET')
 
 alpaca_endpoint = "https://paper-api.alpaca.markets"
 
-api = Trade_api.REST("PKHHGP9WE0PA0XVF54F1", "kXrrs6fYqbCouDvNlkWhIjH82QF4tc48pKaYXvvR", alpaca_endpoint)
+api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
 
 account = api.get_account()
 print(account.status)
