9fdd42e3e967ea4b1ca4c6c66cb7f3f612547164
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-03-26 11:03:30 +0000

    Sorting the Tickers

--- Research/Alpaca_API.py
+++ Research/Alpaca_API.py
@@ -1,11 +1,38 @@
 import alpaca_trade_api as Trade_api
 import os
+import math
+import json
+import time
 
 KEY = os.environ.get('ALPACA_KEY')
 SECRET = os.environ.get('ALPACA_SECRET')
 alpaca_endpoint = "https://paper-api.alpaca.markets"
 
 
+#Open the TickerScores File
+with open(r'BotData\TickerScores2021-03-25.json', 'r') as f:
+    data = json.load(f)
+
+#Empty list 
+sorted_list_keys = []
+
+#Seperate List for the values and the keys within the dictionary
+key_list = list(data.keys())
+value_list = list(data.values())
+
+#sorted list of values
+sorted_values = sorted(value_list, reverse=True)
+
+#sorted list of Keys
+for number in sorted_values:
+    sorted_list_keys.append(key_list[value_list.index(number)])
+
+
+def Calculate_Quantity(price, Buying_Power):
+    #How many whole shares can you buy with your money (Input: Price of one share)
+    quantity = (Buying_Power / price)
+    print(quantity)
+    return quantity
 def Get_Buying_Power():
     api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
 
@@ -17,14 +44,19 @@ def Get_Buying_Power():
         print('Account is currently restricted from trading.')
 
     # Check how much money we have to spend
-    print('${} is available as buying power.'.format(account.buying_power))
+    # print('${} is available as buying power.'.format(account.buying_power))
+    return(account.buying_power)
 def Is_Market_Open():
 
     api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
 
     # Check if the market is open now.
     clock = api.get_clock()
-    print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
+    #print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
+    if clock.is_open:
+        return 1
+    else:
+        return 0
 def HistoricalPrice():
 
     api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
@@ -58,6 +90,40 @@ def View_Positions():
     # Print the quantity of shares for each position.
     for position in portfolio:
         print("{} shares of {}".format(position.qty, position.symbol))
+def Live_Price(Ticker):
+    api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
+    quote = api.get_last_quote(Ticker)
+    return quote.bidprice
 
 
-View_Positions()
+
+while True:
+    if Is_Market_Open() == 0:
+        print("Market is closed....")
+    if Is_Market_Open() == 1:
+        print("Market is now open!!!")
+        while True:
+            
+            with open('Configuration.json', 'r') as f:
+                Data = f.read()
+                Object = json.loads(Data) 
+
+            Buying_power = Get_Buying_Power()
+
+            if Buying_power > 100:
+
+                if Object["Risk_Tolerance"] == 1:
+                    Individual_Stock_Money = Buying_power/5
+                
+                if Object["Risk_Tolerance"] == 2:
+                    Individual_Stock_Money = Buying_power/4
+
+                if Object["Risk_Tolerance"] == 3:
+                    Individual_Stock_Money = Buying_power/3
+
+                if Object["Risk_Tolerance"] == 4:
+                    Individual_Stock_Money = Buying_power/2
+
+                if Object["Risk_Tolerance"] == 5:
+                    Individual_Stock_Money = Buying_power
+    time.sleep(1)
