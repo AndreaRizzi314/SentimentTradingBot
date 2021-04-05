7e0951b10ebe1fb451dc251e19dd36362ce8d3d6
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-04-05 19:52:40 +0100

    Full text problem fixed

--- TwitterAnalysingBot/Twitter_API.py
+++ TwitterAnalysingBot/Twitter_API.py
@@ -39,22 +39,22 @@ class StdOutListener(StreamListener):
 
 
         #Finding user data and tweet text within the json output
-        Object = json.loads(data)
-        user = Object['user'] 
-        text = (Object['text'])
+        Tweet_Object = json.loads(data)
+        user = Tweet_Object['user'] 
+        text = (Tweet_Object['text'])
         followers = user['followers_count']
         
 
         #Determining the influence factor
         with open('Configuration.json', 'r') as f:
             Data = f.read()
-            Object = json.loads(Data) 
-        Hypescore = followers/Object['Hypescore_Formula']
+            Configuration_Object = json.loads(Data) 
+        Hypescore = followers/Configuration_Object['Hypescore_Formula']
 
 
         #Print text of the tweet
         print(text)
-
+        found = False
         #Determine what company the tweet refers to 
         for word in text.split():
 
@@ -62,9 +62,10 @@ class StdOutListener(StreamListener):
 
                 if word.find(Ticker) == -1:
                     pass
-    
+
                 else:
-                    print(Ticker)
+                    print("\nThis tweet is talking about", Ticker)
+                    found = True
                     if TextBlob(text).sentiment.polarity > 0: #If Sentiment is Positive
                         Tickers[Ticker] = Tickers[Ticker] + Hypescore #Add the Hypescore to the total
 
@@ -73,10 +74,40 @@ class StdOutListener(StreamListener):
                     
                     if TextBlob(text).sentiment.polarity == 0: #If Sentiment is neutral
                         Tickers[Ticker] = Tickers[Ticker] + Hypescore #Add the Hypescore to the total
+        
+        if found == False:
+            try:
+                print("\nLooking for full tweet text under retweeted status")
+                full_text = Tweet_Object['retweeted_status']["extended_tweet"]['full_text']
+            except:
+                print("\nFull text in the retweeted status not found")
+                try:
+                    print("\nLooking for full tweet under extended tweet")
+                    full_text = Tweet_Object["extended_tweet"]['full_text']
+                except:
+                    print("\nFull text in the extended tweet not found")
+                    return
+            for word in full_text.split():
+
+                for Ticker in lines:
+
+                    if word.find(Ticker) == -1:
+                        pass
+
+                    else:
+                        print("\nThis tweet is talking about", Ticker)
+                        found = True
+                        if TextBlob(text).sentiment.polarity > 0: #If Sentiment is Positive
+                            Tickers[Ticker] = Tickers[Ticker] + Hypescore #Add the Hypescore to the total
+
+                        if TextBlob(text).sentiment.polarity < 0: #If Sentiment is Negative
+                            Tickers[Ticker] = Tickers[Ticker] - Hypescore #Subtract the Hypescore to the total
+                        
+                        if TextBlob(text).sentiment.polarity == 0: #If Sentiment is neutral
+                            Tickers[Ticker] = Tickers[Ticker] + Hypescore #Add the Hypescore to the total
+
 
         return True
-
-
 
     def on_error(self, status):
         print(status)
