c265c7923aab384237922f041007bc641b4ca60c
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-04-06 10:48:38 +0100

    update

--- Research/InitialConfiguration.py
+++ Research/InitialConfiguration.py
@@ -2,7 +2,7 @@ import json
 
 Config_Dict = {
     'Hypescore_Formula': 100,  #HYPESCORE: followers/?
-    'Diversification': 5,        #DIVERSIFICATION: Number between 1-5 (1 = buy 5 stocks)(5 = buy 1 stock)
+    'Diversification': 5,        #DIVERSIFICATION: Number between 1-5 (1 = buy 1 stock)(5 = buy 5 stocks)
     'Stop_Loss': 2              #WHEN TO SELL(%): Sell when ?% below day highs    
 }
 
