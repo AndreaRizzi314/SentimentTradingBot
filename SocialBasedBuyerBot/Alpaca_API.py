332ad80984160cef29aace945a6d578d076a1a1e
AndreaRizzi314 <andrea_rizzi@outlook.com>
2021-04-06 10:27:53 +0100

    update

--- SocialBasedBuyerBot/Alpaca_API.py
+++ SocialBasedBuyerBot/Alpaca_API.py
@@ -83,14 +83,14 @@ def HistoricalPrice():
     api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
 
     # Get daily price data for AAPL over the last 5 trading days.
-    barset = api.get_barset('DBX', 'day', limit=5)
-    aapl_bars = barset['DBX']
+    barset = api.get_barset('AAPL', 'day', limit=5)
+    aapl_bars = barset['AAPL']
 
     # See how much AAPL moved in that timeframe.
     week_open = aapl_bars[0].o
     week_close = aapl_bars[-1].c
     percent_change = (week_close - week_open) / week_open * 100
-    print('DBX moved {}% over the last 5 days'.format(percent_change))
+    print('AAPL moved {}% over the last 5 days'.format(percent_change))
 def Buy_Order_In_Shares(SYMBOL, QTY):
     api = Trade_api.REST(KEY, SECRET, alpaca_endpoint)
 
